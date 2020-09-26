9610f3f2b2f8771aca40d1cbaee9ad17612edada
cedricfortuinaccommodatiehuur <70884826+cedricfortuinaccommodatiehuur@users.noreply.github.com>
2020-09-26 22:33:43 +0200

    Update config.php

--- website/admin/config/config.php
+++ website/admin/config/config.php
@@ -1,13 +1,14 @@
 <?php
 
+/* Attempt to connect to MySQL database */
+
+//This link is for the database from Het Platenhuis
+
 /*
  * Copyright © 2020 bij Het Platenhuis en Cedric Fortuin. Niks uit deze website mag zonder toestemming gebruikt, gekopieerd en/of verwijderd worden. Als je de website gebruikt ga je akkoord met onze gebruiksvoorwaarden en privacy.
  */
 
-/* Attempt to connect to MySQL database */
-
-//This link is for the database from Het Platenhuis
-$link = mysqli_connect('localhost:3306', 'DatabaseLogin-Platenhuis', 'd2rdS2t0&XBKEJO$4o^Cke3e6LBA6w', 'DATABASE_PLATENHUIS');
+$link = mysqli_connect('localhost:3306', 'DatabaseLogin-Platenhuis', 'Du2wfBtfqUq00FZgso8C', 'DATABASE_PLATENHUIS');
 
 //This link is for the local host
 //$link = mysqli_connect('localhost', 'root', 'root', 'hetplatenhuis' );
